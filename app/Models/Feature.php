7fb7d7a9e9d7edc5b293bd0dad4e04d9a3c0e4f5
Zied Bouazizi <zied.bouazizi@outlook.com>
2025-12-20 12:40:11 +0100

    Improve UI for features and users pages

--- app/Models/Feature.php
+++ app/Models/Feature.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -12,6 +13,23 @@ class Feature extends Model
     use HasFactory;
 
     protected $fillable = ['name', 'description', 'user_id'];
+
+    protected function formatAttribute(string $value): Attribute
+    {
+        return Attribute::make(
+            set: fn($value) => ucfirst(strtolower($value))
+        );
+    }
+
+    protected function name(): Attribute
+    {
+        return $this->formatAttribute('name');
+    }
+
+    protected function description(): Attribute
+    {
+        return $this->formatAttribute('description');
+    }
 
     public function upvotes(): HasMany
     {
